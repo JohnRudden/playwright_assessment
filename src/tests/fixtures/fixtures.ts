52ec6950e9b90cd77ea5a3b39d94e3de1ed98bb4
J.Rudden <52506030+JR-commits@users.noreply.github.com>
2025-03-08 17:55:12 +0000

    removed unused page object file

--- src/tests/fixtures/fixtures.ts
+++ src/tests/fixtures/fixtures.ts
@@ -3,14 +3,12 @@ import SiteNavigation  from '../pageObjects/siteNavigation.page.ts';
 import Header from '../pageObjects/header.page.ts';
 import Documents from '../pageObjects/documents.page.ts';
 import ToastMessage from '../pageObjects/toastMessage.page.ts'; 
-import Timeline from '../pageObjects/timeline.page.ts'
 
 type pageObjects = {
   siteNavigation: SiteNavigation,
   header: Header,
   documents: Documents,
   toastMessage: ToastMessage,
-  // timeline: Timeline
 }
 
 export const test = base.extend<pageObjects>({
@@ -25,10 +23,7 @@ export const test = base.extend<pageObjects>({
   },
   toastMessage: async({page}, use) => {
     await use(new ToastMessage(page))
-  },
-  // timeline: async({page}, use) => {
-  //   await use(new Timeline(page))
-  // }
+  }
 });
 
 export const { Given, When, Then } = createBdd(test);
