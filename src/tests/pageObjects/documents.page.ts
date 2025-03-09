936344a2455e9e1efe5a8350545a8cc814f39525
J.Rudden <52506030+JR-commits@users.noreply.github.com>
2025-03-09 15:11:52 +0000

    added jsdoc comments

--- src/tests/pageObjects/documents.page.ts
+++ src/tests/pageObjects/documents.page.ts
@@ -18,30 +18,54 @@ export default class Documents {
     this.listItem = this.mediaListContainer.getByRole('list');
 }
 
-// actions
+// **** actions ****
 
+/**
+ * Returns the current page number on the search document tab page
+ * @function
+ * @param {string} location - "top" or "bottom" page number selector
+ */
 async getPageNumber(location : string) {
   const locatorToUse = location === "top" ? this.pageNumbersTop : this.pageNumbersBottom
   return await locatorToUse.inputValue();
 }
 
+/**
+ * Returns all the list items on the search document tab page
+ * @function
+ */
 async getAllListItems() {
   const listItems = await this.listItem.all();
-  console.log(listItems.length)
   return listItems;
 }
 
+/**
+ * Returns the current page number on the search document tab page
+ * @function
+ * @param {number} itemNumber - index of item number in the list
+ * @param {string} criteria   - check item number has text based on criteria match
+ */
 async validateListItemNumber(itemNumber: number, criteria: string) {
   const list = await this.getAllListItems();
   expect(list[itemNumber]).toHaveText(criteria);
 }
 
+/**
+ * Validate that the search results have text which match the criteria
+ * @function
+ * @param {string} criteria - Criteria to check the search results against
+ */
 async validateSearchResults(criteria: string) {
   const locateCriteriaItem = this.tabPanel.getByText(criteria);
   await expect(locateCriteriaItem).toBeVisible();
   expect(await locateCriteriaItem.textContent()).toEqual(criteria)
 }
 
+/**
+ * Verify that the results displayed page number matches the value to check for
+ * @function
+ * @param {string | number} checkValue - Criteria to check the search results against. 
+ */
 async verifyDisplayedPageNumber(checkValue: (string | number)) {
   const pageNumberToCheck = typeof checkValue === 'string' ? parseInt(checkValue) : checkValue;
   const actualPageNumberDisplayed = parseInt(await this.getPageNumber('top'));
